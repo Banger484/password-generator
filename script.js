15b8fd8227c49531ef875bbf705530f932c9ca57
Bret Banger <Banger484@gmail.com>
2022-03-23 18:45:29 -0500

    cleaned up character arrays

--- script.js
+++ script.js
@@ -14,15 +14,12 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-var numeric = [0,1,2,3,4,5,6,7,8,9]
-var special = ["!","@","#","$","%","^","&","*","(",")","_","-"]
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercase = []
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var special =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+var numeric = ["0", "1", "2", "3", "4", "5","6","7","8","9"]
 
-for (let i = 0; i < uppercase.length; i++) {
-  lowercase.push(uppercase[i].toLowerCase())
-}
-var charType = [uppercase, lowercase,  numeric, special]
+var charType = [upper, lower,  numeric, special]
 
 function generatePassword() {
   var newPassword = "";
